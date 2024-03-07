f27f6c5d5b7fd630c94bdd910a1f5f61368acdfb
Liam Darkmoon <liamdarkmoon@gmail.com>
2024-03-07 18:11:08 -0300

    asd

--- src/views/landing/LandingPage.jsx
+++ src/views/landing/LandingPage.jsx
@@ -1,10 +1,13 @@
 import { useRef } from "react";
 import Designer from "../../components/pure/Designer";
+import useWindowDimensions from "../../hooks/useDimentions";
 
 const LandingPage = () => {
 
     const projectsRef = useRef(null)
     const sectionRef = useRef(null)
+
+    let w100 = window.innerWidth
 
 
     const sectionSlide = (scrollOffset) => {
@@ -44,12 +47,12 @@ const LandingPage = () => {
 
             <div ref={ projectsRef } className="pb-4">
                 <section ref={ sectionRef } className="col-12 section row flex-nowrap">
-                    <button className="swiper-arrow arrow-left p-0" onClick={ () => sectionSlide(-1143) }>
+                    <button className="swiper-arrow arrow-left p-0" onClick={ () => sectionSlide(w100 >= 720 ? -w100 : null ) }>
                         <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" viewBox="0 0 16 16">
                             <path d="M10 12.796V3.204L4.519 8zm-.659.753-5.48-4.796a1 1 0 0 1 0-1.506l5.48-4.796A1 1 0 0 1 11 3.204v9.592a1 1 0 0 1-1.659.753"/>
                         </svg>
                     </button>
-                    <button className="swiper-arrow arrow-right p-0" onClick={ () => sectionSlide(1143) }>
+                    <button className="swiper-arrow arrow-right p-0" onClick={ () => sectionSlide(w100 >= 720 ? w100 : null) }>
                         <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" viewBox="0 0 16 16">
                             <path d="M6 12.796V3.204L11.481 8zm.659.753 5.48-4.796a1 1 0 0 0 0-1.506L6.66 2.451C6.011 1.885 5 2.345 5 3.204v9.592a1 1 0 0 0 1.659.753"/>
                         </svg>

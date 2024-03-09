c00cce17899e7364be72884cd5ef58b0ecc3a07f
Liam Darkmoon <liamdarkmoon@gmail.com>
2024-03-09 09:16:21 -0300

    About ready

--- src/views/about/AboutPage.jsx
+++ src/views/about/AboutPage.jsx
@@ -1,35 +1,70 @@
 
 const AboutPage = () => {
     return (
-        <div className="pg text-center">
-            <div className="pg-hl pb-4">
-                <h1 className="tf t-pa"> I&apos;m Nahuel Lopez </h1>
-                <h3 className="qf"> Ux Designer & Front-end developer </h3>
+        <main className="pg text-center">
+            <div className="pb-4 pg-hl p-0">
+                <h1 className="tf t-pa pg-title"> Get to know little about me </h1>
             </div>
-            <div className="pg-hl row justify-content-evenly align-items-center">
-                <p className="qf t-sa" style={ {'font-size':'1.5em'} } >
-                    “Enthusiastic about this technological world and everything on it, 
-                    every day on the way to do it better and even more beautiful”
-                </p>
-                <p className="col-12 col-sm-5 about-p">
-                    Hi again I&apos;m Nahuel, 
-                    first time father, always a student, 
-                    a cheerful but somewhat serious guy, 
-                    looking for new projects to change and beautify the world step by step, 
-                    graphics and design are my passion and that&apos;s why I chose this path. 
-                    From both perspectives, 
-                    as a designer and as a developer, 
-                    I try to bring in new ideas and bring a breath of fresh air to every project. Learning from every new situation, 
-                    every new question, 
-                    every doubt or problem is what drives me forward every day. 
-                    For me, there is nothing better or more rewarding than learning day by day and growing both professionally and as a person. 
-                    I love participating in all kinds of projects as much as I love learning. 
-                    So if you are looking for young people with new and fresh perspectives who are ready to give their best, 
-                    then you are in the right place at the right time
-                </p>
-                <img className="col-12 col-sm-5 p-0 border rounded" src="./cvphoto.png" alt="" />
-            </div>
-        </div>
+            <section className="pg-hl pt-0">
+                <h2 className="qf t-sa pb-4">
+                    “Passionate about Crafting Meaningful Digital Experiences”
+                </h2>
+                <article className='row justify-content-evenly align-items-center'>
+                    <p className="col-12 col-sm-6 about-p">
+                        Hi I&apos;m Nahuel, 
+                        first time father, always a student, 
+                        a cheerful but somewhat serious guy, 
+                        looking for new projects to change and beautify the world step by step, 
+                        As a UX/UI Designer and Front-End Developer, 
+                        I am driven by my passion for creating captivating digital experiences that seamlessly blend aesthetics with functionality. 
+                        From both perspectives, 
+                        as a designer and as a developer, 
+                        I bring in new ideas and a breath of fresh air to every project.
+                        With a keen eye for design and a strong foundation in front-end development, 
+                        I strive to deliver solutions that not only look stunning but also provide intuitive and delightful user interactions.
+                    </p>
+                    <img className="col-12 col-sm-4 p-0 border rounded" src="./cvphoto.png" alt="" />
+                </article>
+            </section>
+            <section className='about-section pg-hl col-12 row justify-content-evenly align-content-between'>
+                <div className='col-12 col-sm-5'>
+                    <h4 className='t-sa'>
+                        Holistic Approach: 
+                    </h4>
+                        I believe in taking a holistic approach to design, considering every aspect of the user journey to ensure a cohesive and immersive experience.
+                </div>
+                <div className='col-12 col-sm-5'>
+                    <h4 className='t-sa'>
+                        User-Centered Design: 
+                    </h4>
+                        My designs are always centered around the needs and preferences of the end-users, ensuring that every interaction feels natural and intuitive.
+                </div>
+                <div className='col-12 col-sm-5'>
+                    <h4 className='t-sa'>
+                        Proactive Problem-Solver: 
+                    </h4>
+                        I thrive in dynamic environments and am adept at quickly identifying and solving challenges to keep projects on track and deliver exceptional results.
+                </div>
+                <div className='col-12 col-sm-5'>
+                    <h4 className='t-sa'>
+                        Continuous Learner: 
+                    </h4>
+                        I am committed to staying updated with the latest trends and technologies in the field, constantly expanding my skill set to adapt to evolving industry demands.
+                </div>
+                <div className='col-12 col-sm-5'>
+                    <h4 className='t-sa'>
+                        Technical Proficiency: 
+                    </h4>
+                        With expertise in both UX/UI design principles and front-end development languages such as HTML, CSS, JavaScript, and React js I am equipped to bring my designs to life with precision and efficiency
+                </div>
+
+                <div className='col-10'>
+                    <h5 className='tf t-pa'>
+                        Let&apos;s collaborate to transform your vision into a reality.
+                    </h5>
+                </div>
+            </section>
+        </main>
     );
 }
 
--- src/views/landing/LandingPage.jsx
+++ src/views/landing/LandingPage.jsx
@@ -61,22 +61,21 @@ const LandingPage = () => {
                                     Here i&apos;ve started with a simple Landing page,
                                     with good constrast between components imposed by colors,
                                     while the navbar and all the backgrounds are in this sandy color that make them soft and welcoming,
-                                    the CTAs are bright red inviting you to interacts with them,
-                                    i&apos;ve played a lot too with opacities helping this way the body&apos;s background no&apos;t to be so opresive
-                                    and get some deep.
+                                    the CTAs are bright red inviting you to interact with them,
+                                    i&apos;ve played a lot too with opacities making it more lenient and gaining depth
                                 </p>
                             </div>
-                            <img className="col-12 col-sm-7" src="./landing.jpeg" alt="Landing Page"/>
+                            <img className="project-img col-12 col-sm-7" src="./landing.jpeg" alt="Landing Page"/>
                         </article>
                         <article className="section-article reverse">
-                            <img className="col-8 col-sm-5 mx-sm-0 mx-auto" src="./diceroller.jpeg" alt="Dice Tray" />
+                            <img className="project-img col-8 col-sm-5 mx-sm-0 mx-auto" src="./diceroller.jpeg" alt="Dice Tray" />
                             <div className="col-12 col-sm-4">
                                 <h3 className="tf t-sa"> Dice Tray: the &quot;Product&quot; </h3>
                                 <p>
                                     Landing pages must normaly meet a couple of requirements in order fulfill their mision,
-                                    or at last just to have &quot;good practices&quot;, showcase the product or service is part of these good practices,
-                                    as i show here with the Dice tray a friendly tool for those eager to play rol games right away and without a dice set.
-                                    However his function here is to showcase the product and give the customer a first-hand tryal.
+                                    or at least to comply with the good practices, showcasing the product or service is part of these good practices,
+                                    as i show here with the Dice tray. A friendly tool for those eager to play rol games right away and without a dice set.
+                                    However it&apos;s purpose here is to showcase the product and give the customer a first-hand trial.
                                 </p>
                             </div>
                         </article>
@@ -84,18 +83,22 @@ const LandingPage = () => {
                             <div className="col-12 col-sm-4">
                                 <h3 className="tf t-sa"> Sign Up form </h3>
                                 <p>
-                                    As for the Sign up and sign in forms, those sits on the navbar as buttons,
-                                    they&apos;r poped-up being modal windows helping the user no&apos;t to be forced out of the page,
-                                    neither moved to another one.
+                                    As for the Sign up and sign in forms, those they are dispalyed on the navbar as buttons,
+                                    they&apos;re dispalyed as modal windows as to ease the user to it&apos;s purpose, without interfering with the experience.
                                 </p>
                             </div>
-                            <img  className="col-12 col-sm-7" src="./sign up.jpeg" alt="Sign Up Form"/>
+                            <img  className="project-img col-12 col-sm-7" src="./sign up.jpeg" alt="Sign Up Form"/>
                         </article>
                     </section>
                     <a className="section-link" href="https://liamdarkmoon.github.io/DnDApp/" target="_blank" rel="noopener noreferrer">
                         Click here to visit the App on github
                     </a>
-                    <button className="bttn col-6 col-sm-3" onClick={ scrollToTop }> Go back to the top </button>
+                    <button className="bttn col-6 col-sm-3" onClick={ scrollToTop }>
+                        Go back to the top
+                        <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" viewBox="0 0 16 16">
+                            <path fillRule="evenodd" d="M8 12a.5.5 0 0 0 .5-.5V5.707l2.146 2.147a.5.5 0 0 0 .708-.708l-3-3a.5.5 0 0 0-.708 0l-3 3a.5.5 0 1 0 .708.708L7.5 5.707V11.5a.5.5 0 0 0 .5.5"/>
+                        </svg>
+                    </button>
                 </div>
             </div>
         </div>
